7eefbe7ff629f533df1ad6ea0f2cc6a3fd2e9866
OJOWA AFOLAYAN BAYODE <ojowap61@gmail.com>
2026-08-25 07:52:56 +0100

    fix: add Put import to admin controller

--- apps/admin-service/src/admin.controller.ts
+++ apps/admin-service/src/admin.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Query, Param, Headers, UnauthorizedException } from '@nestjs/common';
+import { Controller, Get, Post, Put, Body, Query, Param, Headers, UnauthorizedException } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { AdminService } from './admin.service';
 
